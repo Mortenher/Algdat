c8e7b40ee1e51752f2e5cc2b3578a68d83e3bf8f
Morten Hermanseter <Morten@Hermanseter.com>
2016-03-31 17:41:59 +0200

    Added files via upload

--- Oblig3_algdat/src/sorting/SortingProgram.java
+++ Oblig3_algdat/src/sorting/SortingProgram.java
@@ -8,32 +8,32 @@ import java.util.Random;
 
 public class SortingProgram {
 	
+	/*
+	 * Sorteringsalgoritmene har jeg ikke laget selv, tenkte jeg skulle gj¯re dette senere.
+	 */
+	
 	
 	static int casenumber;
 	public static final int 
-	MAX_SEQUENTIAL = 100000,
-	MAX_N = 100000000;
+	MAX_SEQUENTIAL = 100000;
 	static int n;
 	
 	
 	public static void insertionSort(int A[])
     {
-	// Innstikksortering av array med heltall
-
+	
 	int n = A.length;
 	int key;
 
 	for (int i = 1; i < n; i++)
-	{
-	    // A er sortert t.o.m. indeks i-1
+	{  
 	    key = A[i];
 	    int j = i;
-	    // Setter element nummer i p√• riktig plass
-	    // blant de i-1 f√∏rste elementene
+	   
 	    while (j > 0 && A[j-1] > key)
 	    {
-		A[j] = A[j-1];
-		j--;
+	    	A[j] = A[j-1];
+	    	j--;
 	    }
 	    A[j] = key;
 	}
@@ -41,24 +41,17 @@ public class SortingProgram {
 	
 	public static void quickSort(int A[], int min, int max)
     {
-		
-	// Quicksort av array med heltall
 
 	int indexofpartition;
 	
 	if (max - min  > 0)
 	{
-	    // Partisjonerer array
 	    indexofpartition = findPartition(A, min, max);
 
-	    // Sorterer venstre del
 	    quickSort(A, min, indexofpartition - 1);
 
-	    // Sorterer h√∏yre del
 	    quickSort(A, indexofpartition + 1, max);
 	    
-	    
-	  
 	}
     }
 	
@@ -68,24 +61,19 @@ public class SortingProgram {
 	int left, right;
 	int temp, partitionelement;
 
-	// Bruker *f√∏rste* element til √• dele opp
 	partitionelement = A[min]; 
 
 	left = min;
 	right = max;
    
-	// Gj√∏r selve partisjoneringen
 	while (left < right)
 	{
-	    // Finn et element som er st√∏rre enn part.elementet
 	    while (A[left] <= partitionelement && left < right)
 		left++;
 
-	    // Finn et element som er mindre enn part.elementet
 	    while (A[right] > partitionelement)
 		right--;
 
-	    // Bytt om de to hvis ikke ferdig
 	    if (left < right)
 	    {
 		temp = A[left];
@@ -94,20 +82,15 @@ public class SortingProgram {
 	    }
 	}
 	
-
-	// Sett part.elementet mellom partisjoneringene
 	temp = A[min];
 	A[min] = A[right];
 	A[right] = temp;
 	
-	// Returner indeksen til part.elementet
 	return right;
     }
 	
     public static void mergeSort (int[] A, int min, int max)
     {
-
-	// Flettesortering av array med heltall
 
 	if (min==max)
 	    return; 
@@ -119,15 +102,12 @@ public class SortingProgram {
 
 	temp = new int[size];
       
-	// Flettesorterer de to halvdelene av arrayen
 	mergeSort(A, min, mid); 
 	mergeSort(A, mid + 1,max);
 
-	// Kopierer array over i temp.array
 	for (index1 = 0; index1 < size; index1++)
 	    temp[index1] = A[min + index1];
       
-	// Fletter sammen de to sorterte halvdelene over i A
 	left = 0;
 	right = mid - min + 1;
 	for (index1 = 0; index1 < size; index1++)
@@ -148,18 +128,15 @@ public class SortingProgram {
 
     public static void radixSort(int[] input) {
     	  final int RADIX = 10;
-    	  // declare and initialize bucket[]
     	  List<Integer>[] bucket = new ArrayList[RADIX];
     	  for (int i = 0; i < bucket.length; i++) {
     	    bucket[i] = new ArrayList<Integer>();
     	  }
     	 
-    	  // sort
     	  boolean maxLength = false;
     	  int tmp = -1, placement = 1;
     	  while (!maxLength) {
     	    maxLength = true;
-    	    // split input between lists
     	    for (Integer i : input) {
     	      tmp = i / placement;
     	      bucket[tmp % RADIX].add(i);
@@ -167,7 +144,6 @@ public class SortingProgram {
     	        maxLength = false;
     	      }
     	    }
-    	    // empty lists into input array
     	    int a = 0;
     	    for (int b = 0; b < RADIX; b++) {
     	      for (Integer i : bucket[b]) {
@@ -175,7 +151,6 @@ public class SortingProgram {
     	      }
     	      bucket[b].clear();
     	    }
-    	    // move to next digit
     	    placement *= RADIX;
     	  }
     	} 
@@ -189,8 +164,10 @@ public class SortingProgram {
     	
     	long time = 0;
     	Random r = new Random();
+    	String test_case = null;
+    	String y = "y";
     	
-		
+		//Leser input fra bruker
 		BufferedReader inn = new BufferedReader( new InputStreamReader(System.in) );
     	inn = new BufferedReader( new InputStreamReader(System.in) );
     	
@@ -203,6 +180,9 @@ public class SortingProgram {
         			+ "Nr 3: Merge Sort. \n"
         			+ "Nr 4: Radix Sort.");
         	casenumber = Integer.parseInt( inn.readLine() );
+        	System.out.println("Vil du teste kj¯ringstid(y) eller arbeidsmengden(n)? Velg y eller n");
+        	test_case = inn.readLine();
+        	
         	
         }
         catch( Exception e ) {
@@ -211,88 +191,134 @@ public class SortingProgram {
 		
        
         A = new int[n];
-       
+       //Switch case som velger hvilken type sortering som skal kj¯res, og om man skal regne ut kj¯ringstid eller arbeidsmengde.
         switch(casenumber){
         	case 1: 
+        		if(test_case.equals(y))
+        		{
 		        	if (n <= MAX_SEQUENTIAL)
 		        	{
-		
 		        	    for (int i = 0; i < n; i++)
 		        	    	A[i] = r.nextInt(n);
 		        	    time = System.currentTimeMillis();
-		        	    double B; 	
 		        	    insertionSort(A);
 		
 		        	    time = System.currentTimeMillis() - time;
-		        	    B = (time/(n*n));
 		        	    System.out.printf("Insertion sort\t: %6.3f s\n", time/1000.0); 
-		        	    System.out.println("Insertion sort\t: " + B);
 		        	}
+
 		        	else
 		        	    System.out.println("O(n^2) sorting too slow for large n");
+        		}
+        		else{
+        			if(n <= MAX_SEQUENTIAL){
+        				for (int i = 0; i < n; i++)
+        			
+	        	    	A[i] = r.nextInt(n);
+	        	    time = System.currentTimeMillis();
+	        	    double B; 	
+	        	    
+	        	    for(int i = 0; i < 5; i++){
+	        	    	insertionSort(A);
+	        	    }
+	
+	        	    time = System.currentTimeMillis() - time;
+	        	    B = (double)(time/(n*n));
+	        	    System.out.println("Insertion sort\t: " + B);
+        			}
+        			else
+        				System.out.println("O(n^2) sorting too slow for large n");
+        		}
+
         			break;
         	case 2: 
-	        		for (int i = 0; i < n; i++)
+        		if(test_case.equals(y))
+        		{
+        			for (int i = 0; i < n; i++)
 	            	    A[i] = r.nextInt(n);
 	            	time = System.currentTimeMillis();
-	            	double C;
 	
 	            	quickSort(A, 0, n-1);
 	                
 	            	
 	            	time = System.currentTimeMillis() - time;
-	            	C = (time/(n*Math.log(n)));
 	            	System.out.printf("Quicksort\t: %6.3f s\n", time/1000.0);
+        		}
+        		else{
+        			for (int i = 0; i < n; i++)
+	            	    A[i] = r.nextInt(n);
+	            	time = System.currentTimeMillis();
+	            	double C;
+	
+	            	for(int i = 0; i < 5; i++){
+	            		quickSort(A, 0, n-1);
+	        	    }
+	            	
+	                
+	            	time = System.currentTimeMillis() - time;
+	            	C = (double)(time/(n*(Math.log(n)/Math.log(2))));
 	            	System.out.println("Quicksort\t: " + C);
+        		}
+	        		
         			break;
         	case 3: 
+        		if(test_case.equals(y))
+        		{
 	        		for (int i = 0; i < n; i++)
 	            	    A[i] = r.nextInt(n);
 	            	time = System.currentTimeMillis();
-	            	double D;
 	            	mergeSort(A, 0, n-1);
 	                
 	            	time = System.currentTimeMillis() - time;
-	            	D = (time/(n*Math.log(n)));
 	            	System.out.printf("Merge sort\t: %6.3f s\n", time/1000.0);
+	                
+	            }
+        		else{
+        			for (int i = 0; i < n; i++)
+	            	    A[i] = r.nextInt(n);
+	            	time = System.currentTimeMillis();
+	            	double D;
+	            	
+	            	for(int i = 0; i < 5; i++){
+	            		mergeSort(A, 0, n-1);
+	        	    }
+	            	
+	                
+	            	time = System.currentTimeMillis() - time;
+	            	D = (double)(time/(Math.log(n)/Math.log(2)));
 	                System.out.println("Merge sort\t: " + D);
+        		}
         			break;
         	case 4: 
+        		if(test_case.equals(y))
+        		{
 	        		for (int i = 0; i < n; i++)
 	            	   A[i] = r.nextInt(n);
 	            	time = System.currentTimeMillis();
-	            	double E;
 	            	radixSort(A);
 	                
 	            	time = System.currentTimeMillis() - time;
-	            	E = (time/n);
 	            	System.out.printf("RadixSort\t: %6.3f s\n", time/1000.0);
-        			System.out.println("RadixSort\t: " + E);
+        		}
+        		else{
+        			
+        			for (int i = 0; i < n; i++)
+ 	            	   A[i] = r.nextInt(n);
+ 	            	time = System.currentTimeMillis();
+ 	            	double E;
+ 	            	
+ 	            	for(int i = 0; i < 5; i++){
+ 	            		radixSort(A);
+	        	    }
+ 	                
+ 	            	time = System.currentTimeMillis() - time;
+ 	            	E = (double)(time/n);
+         			System.out.println("RadixSort\t: " + E);
+        		}
         			break;
-        	default: System.out.println("Velg en gylding funksjon");
+        	default: System.out.println("Velg en gyldig funksjon");
         			break;
-    	}
-        
-        
-        
-        
-    	
-        /*try
-    	{
-    	   //  if (args.length != 1)
-    		//  throw new IOException("Please provide one integer");
-    	  //   n = Integer.parseInt(args[0]);
-    	     if (n < 0 || n > MAX_N)
-    		  throw new IOException("Illegal value of n: " + n +
-                                          ". Use  0 < " + n + " < " + MAX_N);
-           	}
-            catch (Exception e)
-    	{
-    	    System.err.println(e);
-    	    System.exit(1);
-    	}*/
-        
-        
+    	}   
 	}
 }       
 	
